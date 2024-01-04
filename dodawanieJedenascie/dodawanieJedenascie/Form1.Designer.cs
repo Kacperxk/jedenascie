dfd48309baba95c740586879e7ac3e65176ae0dd
Kacperxk <145069408+Kacperxk@users.noreply.github.com>
2024-01-04 16:32:49 +0100

    Add files via upload

---
+++ dodawanieJedenascie/dodawanieJedenascie/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿
+namespace dodawanieJedenascie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.liczba1 = new System.Windows.Forms.Label();
+            this.liczba2 = new System.Windows.Forms.Label();
+            this.p2 = new System.Windows.Forms.TextBox();
+            this.p1 = new System.Windows.Forms.TextBox();
+            this.p3 = new System.Windows.Forms.TextBox();
+            this.p4 = new System.Windows.Forms.TextBox();
+            this.p5 = new System.Windows.Forms.TextBox();
+            this.l1 = new System.Windows.Forms.TextBox();
+            this.l2 = new System.Windows.Forms.TextBox();
+            this.l3 = new System.Windows.Forms.TextBox();
+            this.l4 = new System.Windows.Forms.TextBox();
+            this.l5 = new System.Windows.Forms.TextBox();
+            this.l6 = new System.Windows.Forms.TextBox();
+            this.wynik = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // liczba1
+            // 
+            this.liczba1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.liczba1.Location = new System.Drawing.Point(237, 154);
+            this.liczba1.Name = "liczba1";
+            this.liczba1.Size = new System.Drawing.Size(337, 40);
+            this.liczba1.TabIndex = 0;
+            this.liczba1.Text = "7 6 3 2 9";
+            this.liczba1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // liczba2
+            // 
+            this.liczba2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.liczba2.Location = new System.Drawing.Point(262, 205);
+            this.liczba2.Name = "liczba2";
+            this.liczba2.Size = new System.Drawing.Size(283, 40);
+            this.liczba2.TabIndex = 1;
+            this.liczba2.Text = "+ 7 6 3 2 9";
+            this.liczba2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // p2
+            // 
+            this.p2.Location = new System.Drawing.Point(390, 132);
+            this.p2.MaxLength = 1;
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(20, 20);
+            this.p2.TabIndex = 3;
+            this.p2.TextChanged += new System.EventHandler(this.p2_TextChanged);
+            // 
+            // p1
+            // 
+            this.p1.Location = new System.Drawing.Point(421, 132);
+            this.p1.MaxLength = 1;
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(20, 20);
+            this.p1.TabIndex = 4;
+            this.p1.TextChanged += new System.EventHandler(this.p1_TextChanged);
+            // 
+            // p3
+            // 
+            this.p3.Location = new System.Drawing.Point(360, 132);
+            this.p3.MaxLength = 1;
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(20, 20);
+            this.p3.TabIndex = 5;
+            this.p3.TextChanged += new System.EventHandler(this.p3_TextChanged);
+            // 
+            // p4
+            // 
+            this.p4.Location = new System.Drawing.Point(329, 132);
+            this.p4.MaxLength = 1;
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(20, 20);
+            this.p4.TabIndex = 6;
+            this.p4.TextChanged += new System.EventHandler(this.p4_TextChanged);
+            // 
+            // p5
+            // 
+            this.p5.Location = new System.Drawing.Point(300, 132);
+            this.p5.MaxLength = 1;
+            this.p5.Name = "p5";
+            this.p5.Size = new System.Drawing.Size(20, 20);
+            this.p5.TabIndex = 7;
+            this.p5.TextChanged += new System.EventHandler(this.p5_TextChanged);
+            // 
+            // l1
+            // 
+            this.l1.Location = new System.Drawing.Point(453, 248);
+            this.l1.MaxLength = 1;
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(20, 20);
+            this.l1.TabIndex = 8;
+            this.l1.TextChanged += new System.EventHandler(this.l1_TextChanged);
+            // 
+            // l2
+            // 
+            this.l2.Location = new System.Drawing.Point(421, 248);
+            this.l2.MaxLength = 1;
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(20, 20);
+            this.l2.TabIndex = 9;
+            this.l2.TextChanged += new System.EventHandler(this.l2_TextChanged);
+            // 
+            // l3
+            // 
+            this.l3.Location = new System.Drawing.Point(390, 248);
+            this.l3.MaxLength = 1;
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(20, 20);
+            this.l3.TabIndex = 10;
+            this.l3.TextChanged += new System.EventHandler(this.l3_TextChanged);
+            // 
+            // l4
+            // 
+            this.l4.Location = new System.Drawing.Point(360, 248);
+            this.l4.MaxLength = 1;
+            this.l4.Name = "l4";
+            this.l4.Size = new System.Drawing.Size(20, 20);
+            this.l4.TabIndex = 11;
+            this.l4.TextChanged += new System.EventHandler(this.l4_TextChanged);
+            // 
+            // l5
+            // 
+            this.l5.Location = new System.Drawing.Point(329, 248);
+            this.l5.MaxLength = 1;
+            this.l5.Name = "l5";
+            this.l5.Size = new System.Drawing.Size(20, 20);
+            this.l5.TabIndex = 12;
+            this.l5.TextChanged += new System.EventHandler(this.l5_TextChanged);
+            // 
+            // l6
+            // 
+            this.l6.Location = new System.Drawing.Point(300, 248);
+            this.l6.MaxLength = 1;
+            this.l6.Name = "l6";
+            this.l6.Size = new System.Drawing.Size(20, 20);
+            this.l6.TabIndex = 13;
+            this.l6.TextChanged += new System.EventHandler(this.l6_TextChanged);
+            // 
+            // wynik
+            // 
+            this.wynik.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.wynik.Location = new System.Drawing.Point(12, 312);
+            this.wynik.Name = "wynik";
+            this.wynik.Size = new System.Drawing.Size(701, 49);
+            this.wynik.TabIndex = 14;
+            this.wynik.Text = "Wynik";
+            this.wynik.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // reset
+            // 
+            this.reset.BackColor = System.Drawing.Color.White;
+            this.reset.Location = new System.Drawing.Point(599, 12);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(189, 74);
+            this.reset.TabIndex = 15;
+            this.reset.Text = "RESET";
+            this.reset.UseVisualStyleBackColor = false;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.wynik);
+            this.Controls.Add(this.l6);
+            this.Controls.Add(this.l5);
+            this.Controls.Add(this.l4);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.l1);
+            this.Controls.Add(this.p5);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.liczba2);
+            this.Controls.Add(this.liczba1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label liczba1;
+        private System.Windows.Forms.Label liczba2;
+        private System.Windows.Forms.TextBox p2;
+        private System.Windows.Forms.TextBox p1;
+        private System.Windows.Forms.TextBox p3;
+        private System.Windows.Forms.TextBox p4;
+        private System.Windows.Forms.TextBox p5;
+        private System.Windows.Forms.TextBox l1;
+        private System.Windows.Forms.TextBox l2;
+        private System.Windows.Forms.TextBox l3;
+        private System.Windows.Forms.TextBox l4;
+        private System.Windows.Forms.TextBox l5;
+        private System.Windows.Forms.TextBox l6;
+        private System.Windows.Forms.Label wynik;
+        private System.Windows.Forms.Button reset;
+    }
+}
+
---
+++ dodawanieJedenascie/dodawanieJedenascie/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dodawanieJedenascie
+{
+    public partial class Form1 : Form
+    {
+        int[] liczby = new int[10];
+        private void przygotuj()
+        {
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                liczby[i] = 0;
+            }
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int temp = rand.Next(0, 10);
+                liczby[i] = temp;
+            }
+            string licz1 = "";
+            string licz2 = " ";
+            for (int i = 0; i < 10; i++)
+            {
+                if (i < 4)
+                {
+                    licz1 += liczby[i] + " ";
+                }
+                else if (i == 4)
+                {
+                    licz1 += liczby[i];
+                }
+                else if (i < 10)
+                {
+                    licz2 += liczby[i] + " ";
+                }
+                else if (i == 10)
+                {
+                    licz2 += liczby[i];
+                }
+            }
+            liczba1.Text = licz1;
+            liczba2.Text = licz2;
+            l2.Enabled = false;
+            l3.Enabled = false;
+            l4.Enabled = false;
+            l5.Enabled = false;
+            l6.Enabled = false;
+            p2.Enabled = false;
+            p3.Enabled = false;
+            p4.Enabled = false;
+            p5.Enabled = false;
+            l1.Text = "";
+            l2.Text = "";
+            l3.Text = "";
+            l4.Text = "";
+            l5.Text = "";
+            l6.Text = "";
+            p1.Text = "";
+            p2.Text = "";
+            p3.Text = "";
+            p4.Text = "";
+            p5.Text = "";
+            wynik.Text = "Oblicz wynik!";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            przygotuj();
+            //label1.Text = liczby[4] + " " + liczby[9];
+        }
+        private void checker1()
+        {
+            int suma = liczby[4] + liczby[9];
+            if (l1.Text == null) { return; }
+            if (l1.Text == "") { return; }
+            if (p1.Text == null) { return; }
+            if (p1.Text == "") { return; }
+            if(l1.Text == "A") 
+            {
+                if (suma % 11 == 10 && (suma / 11).ToString() == p1.Text)
+                {
+                    l2.Enabled = true;
+                    p2.Enabled = true;
+                }
+            } else
+            {
+                if (suma % 11 == Convert.ToInt32(l1.Text) && (suma / 11).ToString() == p1.Text)
+                {
+                    l2.Enabled = true;
+                    p2.Enabled = true;
+                }
+            }
+
+
+           
+        }
+        private void checker2()
+        {
+            if (l2.Text == null) { return; }
+            if (l2.Text == "") { return; }
+            if (p2.Text == null) { return; }
+            if (p2.Text == "") { return; }
+
+            int suma = liczby[3] + liczby[8] + Convert.ToInt32(p1.Text);
+            if (l2.Text == "A")
+            {
+                if (suma % 11 == 10 && (suma / 11).ToString() == p2.Text)
+                {
+                    l3.Enabled = true;
+                    p3.Enabled = true;
+                }
+            }
+            else
+            {
+                if (suma % 11 == Convert.ToInt32(l2.Text) && (suma / 11).ToString() == p2.Text)
+                {
+                    l3.Enabled = true;
+                    p3.Enabled = true;
+                }
+            }
+        }
+        private void checker3()
+        {
+            if (l3.Text == null) { return; }
+            if (l3.Text == "") { return; }
+            if (p3.Text == null) { return; }
+            if (p3.Text == "") { return; }
+
+            int suma = liczby[2] + liczby[7] + Convert.ToInt32(p2.Text);
+            if (l3.Text == "A")
+            {
+                if (suma % 11 == 10 && (suma / 11).ToString() == p3.Text)
+                {
+                    l4.Enabled = true;
+                    p4.Enabled = true;
+                }
+            }
+            else
+            {
+                if (suma % 11 == Convert.ToInt32(l3.Text) && (suma / 11).ToString() == p3.Text)
+                {
+                    l4.Enabled = true;
+                    p4.Enabled = true;
+                }
+            }
+        }
+        private void checker4()
+        {
+            if (l4.Text == null) { return; }
+            if (l4.Text == "") { return; }
+            if (p4.Text == null) { return; }
+            if (p4.Text == "") { return; }
+
+            int suma = liczby[1] + liczby[6] + Convert.ToInt32(p3.Text);
+            if (l4.Text == "A")
+            {
+                if (suma % 11 == 10 && (suma / 11).ToString() == p4.Text)
+                {
+                    l5.Enabled = true;
+                    p5.Enabled = true;
+                }
+            }
+            else
+            {
+                if (suma % 11 == Convert.ToInt32(l4.Text) && (suma / 11).ToString() == p4.Text)
+                {
+                    l5.Enabled = true;
+                    p5.Enabled = true;
+                }
+            }
+        }
+        private void checker5()
+        {
+            if (l5.Text == null) { return; }
+            if (l5.Text == "") { return; }
+            if (p5.Text == null) { return; }
+            if (p5.Text == "") { return; }
+
+            int suma = liczby[0] + liczby[5] + Convert.ToInt32(p4.Text);
+            if (l5.Text == "A")
+            {
+                if (suma % 11 == 10 && (suma / 11).ToString() == p5.Text)
+                {
+                    l6.Enabled = true;
+                }
+            }
+            else
+            {
+                if (suma % 11 == Convert.ToInt32(l5.Text) && (suma / 11).ToString() == p5.Text)
+                {
+                    l6.Enabled = true;
+                }
+            }
+        }
+        private void l1_TextChanged(object sender, EventArgs e)
+        {
+            checker1();
+        }
+        private void p1_TextChanged(object sender, EventArgs e)
+        {
+            checker1();
+        }
+        private void l2_TextChanged(object sender, EventArgs e)
+        {
+            checker2();
+        }
+
+        private void p2_TextChanged(object sender, EventArgs e)
+        {
+            checker2();
+        }
+
+        private void l3_TextChanged(object sender, EventArgs e)
+        {
+            checker3();
+        }
+
+        private void p3_TextChanged(object sender, EventArgs e)
+        {
+            checker3();
+        }
+
+        private void l4_TextChanged(object sender, EventArgs e)
+        {
+            checker4();
+        }
+
+        private void p4_TextChanged(object sender, EventArgs e)
+        {
+            checker4();
+        }
+
+        private void l5_TextChanged(object sender, EventArgs e)
+        {
+            checker5();
+        }
+
+        private void p5_TextChanged(object sender, EventArgs e)
+        {
+            checker5();
+        }
+        private void koniec(int jest)
+        {
+            if (jest == 1)
+            {
+                string Wynik = l5.Text + l4.Text + l3.Text + l2.Text + l1.Text;
+                wynik.Text = "Udało ci się obliczyć działanie! Jest to : " + Wynik;
+            }
+            else
+            {
+                string Wynik = l6.Text + l5.Text + l4.Text + l3.Text + l2.Text + l1.Text;
+                wynik.Text = "Udało ci się obliczyć działanie! Jest to : " + Wynik;
+            }
+            reset.Enabled = true;
+        }
+        private void l6_TextChanged(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(p5.Text) != 0)
+            {
+                if (l6.Text == p5.Text)
+                {
+                    koniec(0);
+                }
+            }
+            else
+            {
+                if (l6.Text == p5.Text)
+                {
+                    koniec(1);
+                }
+            }
+        }
+        private void l1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void l2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void l3_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void l4_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void l5_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void l6_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void p1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void p2_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void p3_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void p4_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void p5_MaskInputRejected(object sender, MaskInputRejectedEventArgs e) { ToolTip t1 = new ToolTip(); t1.ToolTipTitle = "Błąd wprowadzania"; t1.Show("Wprowadź liczbę od 0 do 9", l1, 0, -20, 5000); }
+        private void Form1_Load(object sender, EventArgs e) { }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //wynik.Text = "X: " + e.X + ", Y: " + e.Y;
+        }
+        private void reset_Click(object sender, EventArgs e)
+        {
+            przygotuj();
+            Invalidate();
+            Update();
+        }
+    }
+}
